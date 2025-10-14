325ad00b5024637299e45f429b63cb9feb788358
John Salvesen <john.salvesen@cern.ch>
2025-10-14 16:30:44 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ with open("README.md", "r") as f:
 setup(
     name                            = "sad2xs",
     version                         = "0.1.6",
-    date                            = "09-10-2025",
+    date                            = "14-10-2025",
     description                     = "Conversion of SAD lattices to Xtrack format",
     long_description                = description,
     long_description_content_type   = "text/markdown",
