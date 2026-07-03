4af4ce7821d85e2b244955e2c7e561d290c357eb
Jack Salvesen <70807974+JPTS2@users.noreply.github.com>
2026-07-03 19:13:47 +0200

    Integrator and model setting

--- sad2xs/output_writer/_014_model.py
+++ sad2xs/output_writer/_014_model.py
@@ -62,6 +62,8 @@ line.set(
     num_multipole_kicks = {config.N_INTEGRATOR_KICKS_OCT})
 line.set(
     tt_mult,
+    model               = "{config.MODEL_MULT}",
+    integrator          = "{config.INTEGRATOR_MULT}",
     num_multipole_kicks = {config.N_INTEGRATOR_KICKS_MULT})
 line.set(
     tt_sol,
