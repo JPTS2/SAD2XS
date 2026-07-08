Fix the rotation of bends and correctors to the converter not the output writer This is a SAD2XS convention, not required for other lattiecs going in with future serialisation

sad2xs/output_writer/_002_bend.py
@@ -97,18 +97,24 @@ env.new(name = '{sbend_name}', prototype = xt.Bend, length = {sbend_length})"""
 
     for hbend, hbend_length in zip(hbend_names, hbend_lengths):
         for replica_name in hbends[hbend_length]:
-            replica_variable    = bend_name_dict[replica_name]
+            source_name         = replica_name
+            replica_variable    = bend_name_dict[source_name]
+            bend                = line[source_name]
+            angle               = bend.angle
+            edge_entry_angle    = bend.edge_entry_angle
+            edge_exit_angle     = bend.edge_exit_angle
 
             # Remove the minus sign if no non minus version exists
             if replica_name.startswith("-"):
-                root_name   = replica_name[1:]
-                if root_name not in hbends[hbend_length]:
-                    replica_name        = root_name
+                root_name    = replica_name[1:]
+                source_names = hbends[hbend_length]
+                if root_name not in source_names:
+                    replica_name = root_name
 
             # If simple try to make it more compact
-            if check_is_simple_bend_corr(line, replica_name):
+            if check_is_simple_bend_corr(line, source_name):
                 bend_generation = f"""
-env.new(name = '{replica_name}', prototype = '{hbend}', angle = {line[replica_name].angle:.24f}, k0 = 'k0_{replica_variable}')"""
+env.new(name = '{replica_name}', prototype = '{hbend}', angle = {angle:.24f}, k0 = 'k0_{replica_variable}')"""
 
             # Otherwise do the full version
             else:
@@ -116,34 +122,34 @@ env.new(name = '{replica_name}', prototype = '{hbend}', angle = {line[replica_na
 env.new(
     name                    = '{replica_name}',
     prototype               = '{hbend}',
-    angle                   = {line[replica_name].angle:.24f},
+    angle                   = {angle:.24f},
     k0                      = 'k0_{replica_variable}'"""
-                if line[replica_name].k1 != 0:
+                if bend.k1 != 0:
                     bend_generation += f""",
     k1                      = 'k1_{replica_variable}'"""
             # Append edge entry angles
-                if line[replica_name].edge_entry_angle != 0:
+                if bend.edge_entry_angle != 0:
                     bend_generation += f""",
-    edge_entry_angle        = {line[replica_name].edge_entry_angle}"""
-                if line[replica_name].edge_exit_angle != 0:
+    edge_entry_angle        = {edge_entry_angle}"""
+                if bend.edge_exit_angle != 0:
                     bend_generation += f""",
-    edge_exit_angle         = {line[replica_name].edge_exit_angle}"""
-                if line[replica_name].edge_entry_angle_fdown != 0:
+    edge_exit_angle         = {edge_exit_angle}"""
+                if bend.edge_entry_angle_fdown != 0:
                     bend_generation += f""",
-    edge_entry_angle_fdown  = {line[replica_name].edge_entry_angle_fdown}"""
-                if line[replica_name].edge_exit_angle_fdown != 0:
+    edge_entry_angle_fdown  = {bend.edge_entry_angle_fdown}"""
+                if bend.edge_exit_angle_fdown != 0:
                     bend_generation += f""",
-    edge_exit_angle_fdown   = {line[replica_name].edge_exit_angle_fdown}"""
+    edge_exit_angle_fdown   = {bend.edge_exit_angle_fdown}"""
                 # Append shifts if they exist
-                if line[replica_name].shift_x != 0:
+                if bend.shift_x != 0:
                     bend_generation += f""",
-    shift_x                 = '{line[replica_name].shift_x}'"""
-                if line[replica_name].shift_y != 0:
+    shift_x                 = '{bend.shift_x}'"""
+                if bend.shift_y != 0:
                     bend_generation += f""",
-    shift_y                 = '{line[replica_name].shift_y}'"""
+    shift_y                 = '{bend.shift_y}'"""
                 # Combined multipole components
-                knl_str = get_knl_string(np.asarray(line[replica_name].knl))
-                ksl_str = get_knl_string(np.asarray(line[replica_name].ksl))
+                knl_str = get_knl_string(np.asarray(bend.knl))
+                ksl_str = get_knl_string(np.asarray(bend.ksl))
                 if knl_str != "[]":
                     bend_generation += f""",
     knl                     = {knl_str}"""
@@ -159,18 +165,24 @@ env.new(
 
     for vbend, vbend_length in zip(vbend_names, vbend_lengths):
         for replica_name in vbends[vbend_length]:
-            replica_variable    = bend_name_dict[replica_name]
+            source_name         = replica_name
+            replica_variable    = bend_name_dict[source_name]
+            bend                = line[source_name]
+            angle               = bend.angle
+            edge_entry_angle    = bend.edge_entry_angle
+            edge_exit_angle     = bend.edge_exit_angle
 
             # Remove the minus sign if no non minus version exists
             if replica_name.startswith("-"):
-                root_name   = replica_name[1:]
-                if root_name not in vbends[vbend_length]:
-                    replica_name        = root_name
+                root_name    = replica_name[1:]
+                source_names = vbends[vbend_length]
+                if root_name not in source_names:
+                    replica_name = root_name
 
             # If simple try to make it more compact
-            if check_is_simple_bend_corr(line, replica_name):
+            if check_is_simple_bend_corr(line, source_name):
                 bend_generation = f"""
-env.new(name = '{replica_name}', prototype = '{vbend}', angle = {line[replica_name].angle:.24f}, k0 = 'k0_{replica_variable}')"""
+env.new(name = '{replica_name}', prototype = '{vbend}', angle = {angle:.24f}, k0 = 'k0_{replica_variable}')"""
 
             # Otherwise do the full version
             else:
@@ -178,34 +190,34 @@ env.new(name = '{replica_name}', prototype = '{vbend}', angle = {line[replica_na
 env.new(
     name                    = '{replica_name}',
     prototype               = '{vbend}',
-    angle                   = {line[replica_name].angle:.24f},
+    angle                   = {angle:.24f},
     k0                      = 'k0_{replica_variable}'"""
-                if line[replica_name].k1 != 0:
+                if bend.k1 != 0:
                     bend_generation += f""",
     k1                      = 'k1_{replica_variable}'"""
             # Append edge entry angles
-                if line[replica_name].edge_entry_angle != 0:
+                if bend.edge_entry_angle != 0:
                     bend_generation += f""",
-    edge_entry_angle        = {line[replica_name].edge_entry_angle}"""
-                if line[replica_name].edge_exit_angle != 0:
+    edge_entry_angle        = {edge_entry_angle}"""
+                if bend.edge_exit_angle != 0:
                     bend_generation += f""",
-    edge_exit_angle         = {line[replica_name].edge_exit_angle}"""
-                if line[replica_name].edge_entry_angle_fdown != 0:
+    edge_exit_angle         = {edge_exit_angle}"""
+                if bend.edge_entry_angle_fdown != 0:
                     bend_generation += f""",
-    edge_entry_angle_fdown  = {line[replica_name].edge_entry_angle_fdown}"""
-                if line[replica_name].edge_exit_angle_fdown != 0:
+    edge_entry_angle_fdown  = {bend.edge_entry_angle_fdown}"""
+                if bend.edge_exit_angle_fdown != 0:
                     bend_generation += f""",
-    edge_exit_angle_fdown   = {line[replica_name].edge_exit_angle_fdown}"""
+    edge_exit_angle_fdown   = {bend.edge_exit_angle_fdown}"""
                 # Append shifts if they exist
-                if line[replica_name].shift_x != 0:
+                if bend.shift_x != 0:
                     bend_generation += f""",
-    shift_x                 = '{line[replica_name].shift_x}'"""
-                if line[replica_name].shift_y != 0:
+    shift_x                 = '{bend.shift_x}'"""
+                if bend.shift_y != 0:
                     bend_generation += f""",
-    shift_y                 = '{line[replica_name].shift_y}'"""
+    shift_y                 = '{bend.shift_y}'"""
                 # Combined multipole components
-                knl_str = get_knl_string(np.asarray(line[replica_name].knl))
-                ksl_str = get_knl_string(np.asarray(line[replica_name].ksl))
+                knl_str = get_knl_string(np.asarray(bend.knl))
+                ksl_str = get_knl_string(np.asarray(bend.ksl))
                 if knl_str != "[]":
                     bend_generation += f""",
     knl                     = {knl_str}"""
@@ -220,18 +232,25 @@ env.new(
 
     for sbend, sbend_length in zip(sbend_names, sbend_lengths):
         for replica_name in sbends[sbend_length]:
-            replica_variable    = bend_name_dict[replica_name]
+            source_name         = replica_name
+            replica_variable    = bend_name_dict[source_name]
+            bend                = line[source_name]
+            angle               = bend.angle
+            edge_entry_angle    = bend.edge_entry_angle
+            edge_exit_angle     = bend.edge_exit_angle
+            rot_s_rad           = bend.rot_s_rad
 
             # Remove the minus sign if no non minus version exists
             if replica_name.startswith("-"):
-                root_name   = replica_name[1:]
-                if root_name not in sbends[sbend_length]:
-                    replica_name        = root_name
+                root_name    = replica_name[1:]
+                source_names = sbends[sbend_length]
+                if root_name not in source_names:
+                    replica_name = root_name
 
             # If simple try to make it more compact
-            if check_is_simple_bend_corr(line, replica_name):
+            if check_is_simple_bend_corr(line, source_name):
                 bend_generation = f"""
-env.new(name = '{replica_name}', prototype = '{sbend}', angle = {line[replica_name].angle:.24f}, k0 = 'k0_{replica_variable}', rot_s_rad = '{line[replica_name].rot_s_rad}')"""
+env.new(name = '{replica_name}', prototype = '{sbend}', angle = {angle:.24f}, k0 = 'k0_{replica_variable}', rot_s_rad = '{rot_s_rad}')"""
 
             # Otherwise do the full version
             else:
@@ -239,37 +258,37 @@ env.new(name = '{replica_name}', prototype = '{sbend}', angle = {line[replica_na
 env.new(
     name                    = '{replica_name}',
     prototype               = '{sbend}',
-    angle                   = {line[replica_name].angle:.24f},
+    angle                   = {angle:.24f},
     k0                      = 'k0_{replica_variable}'"""
-                if line[replica_name].k1 != 0:
+                if bend.k1 != 0:
                     bend_generation += f""",
     k1                      = 'k1_{replica_variable}'"""
             # Append edge entry angles
-                if line[replica_name].edge_entry_angle != 0:
+                if bend.edge_entry_angle != 0:
                     bend_generation += f""",
-    edge_entry_angle        = {line[replica_name].edge_entry_angle}"""
-                if line[replica_name].edge_exit_angle != 0:
+    edge_entry_angle        = {edge_entry_angle}"""
+                if bend.edge_exit_angle != 0:
                     bend_generation += f""",
-    edge_exit_angle         = {line[replica_name].edge_exit_angle}"""
-                if line[replica_name].edge_entry_angle_fdown != 0:
+    edge_exit_angle         = {edge_exit_angle}"""
+                if bend.edge_entry_angle_fdown != 0:
                     bend_generation += f""",
-    edge_entry_angle_fdown  = {line[replica_name].edge_entry_angle_fdown}"""
-                if line[replica_name].edge_exit_angle_fdown != 0:
+    edge_entry_angle_fdown  = {bend.edge_entry_angle_fdown}"""
+                if bend.edge_exit_angle_fdown != 0:
                     bend_generation += f""",
-    edge_exit_angle_fdown   = {line[replica_name].edge_exit_angle_fdown}"""
+    edge_exit_angle_fdown   = {bend.edge_exit_angle_fdown}"""
                 # Append shifts if they exist
-                if line[replica_name].shift_x != 0:
+                if bend.shift_x != 0:
                     bend_generation += f""",
-    shift_x                 = '{line[replica_name].shift_x}'"""
-                if line[replica_name].shift_y != 0:
+    shift_x                 = '{bend.shift_x}'"""
+                if bend.shift_y != 0:
                     bend_generation += f""",
-    shift_y                 = '{line[replica_name].shift_y}'"""
+    shift_y                 = '{bend.shift_y}'"""
             # In the case of a skew bend, we need to add a rotation
                 bend_generation += f""",
-    rot_s_rad               = '{line[replica_name].rot_s_rad}'"""
+    rot_s_rad               = '{rot_s_rad}'"""
                 # Combined multipole components
-                knl_str = get_knl_string(np.asarray(line[replica_name].knl))
-                ksl_str = get_knl_string(np.asarray(line[replica_name].ksl))
+                knl_str = get_knl_string(np.asarray(bend.knl))
+                ksl_str = get_knl_string(np.asarray(bend.ksl))
                 if knl_str != "[]":
                     bend_generation += f""",
     knl                     = {knl_str}"""
@@ -336,21 +355,20 @@ def create_bend_optics_file_information(
         k1 = None
 
         try:
-            if line[bend_variable].k0_from_h is True:
-                k0  = line[bend_variable].angle / line[bend_variable].length
-            else:
-                k0  = line[bend_variable].k0
-            k1  = line[bend_variable].k1
+            bend = line[bend_variable]
         except KeyError:
             try:
-                if line[f"-{bend_variable}"].k0_from_h is True:
-                    k0  = line[f"-{bend_variable}"].angle / line[f"-{bend_variable}"].length
-                else:
-                    k0  = line[f"-{bend_variable}"].k0
-                k1  = line[f"-{bend_variable}"].k1
+                bend = line[f"-{bend_variable}"]
             except KeyError as exc:
                 raise KeyError(
-                    f"Could not find bend variable {bend_variable} or -{bend_variable} in line.") from exc
+                    f"Could not find bend variable {bend_variable} or "
+                    f"-{bend_variable} in line.") from exc
+
+        if bend.k0_from_h is True:
+            k0 = bend.angle / bend.length
+        else:
+            k0 = bend.k0
+        k1 = bend.k1
 
         if k0 == 0:
             k0 = None

tests/conversion/elements/test_corrector.py
@@ -333,7 +333,7 @@ def test_corrector_converter_creates_all_correctors(
         "All parsed SAD correctors should be present in the environment.")
     for corr_name, expected_k0 in [
             ("ch", 0.2),
-            ("cv", -0.2),
+            ("cv", 0.2),
             ("cz", 0.0)]:
         corrector = assert_environment_element(
             environment     = xsuite_environment,
@@ -433,13 +433,26 @@ def test_corrector_converter_supports_symbolic_length_and_strength(
 ########################################
 # Offsets and Rotations
 ########################################
-def test_corrector_converter_preserves_offsets_and_rotation(
+@pytest.mark.parametrize(
+    "sad_rotation, expected_rotation, expected_sign",
+    [
+        (0.0,        0.0,       +1),
+        (+np.pi,     0.0,       -1),
+        (-np.pi,     0.0,       -1),
+        (+np.pi / 2, np.pi / 2, -1),
+        (-np.pi / 2, np.pi / 2, +1),
+        (0.125,     -0.125,     +1),
+    ])
+def test_corrector_converter_canonicalizes_dipole_rotations(
         parsed_elements,
         xsuite_environment,
         sad2xs_config,
-        assert_environment_element):
+        assert_environment_element,
+        sad_rotation,
+        expected_rotation,
+        expected_sign):
     """
-    SAD corrector DX, DY, and ROTATE should map to Xsuite element fields.
+    SAD corrector special rotations should map to canonical Xsuite Bend fields.
     """
     convert_correctors(
         parsed_elements = parsed_elements(
@@ -451,7 +464,7 @@ def test_corrector_converter_preserves_offsets_and_rotation(
                 "k0":       0.1,
                 "dx":       1.0E-3,
                 "dy":       -2.0E-3,
-                "rotate":   np.pi / 2,
+                "rotate":   sad_rotation,
             }),
         environment     = xsuite_environment,
         config          = sad2xs_config)
@@ -465,8 +478,56 @@ def test_corrector_converter_preserves_offsets_and_rotation(
         "Converted corrector should preserve SAD DX as Xsuite shift_x.")
     assert corrector.shift_y == pytest.approx(-2.0E-3), (
         "Converted corrector should preserve SAD DY as Xsuite shift_y.")
-    assert corrector.rot_s_rad == pytest.approx(-np.pi / 2), (
-        "Converted corrector should apply the SAD-to-Xsuite rotation sign.")
+    assert corrector.k0 == pytest.approx(expected_sign * 0.2), (
+        "Canonicalized corrector k0 should include the dipole field sign.")
+    assert corrector.rot_s_rad == pytest.approx(expected_rotation), (
+        "Converted corrector should use the canonical Xsuite dipole rotation.")
+
+@pytest.mark.parametrize(
+    "sad_rotation, expected_rotation, expected_sign",
+    [
+        (0.0,        0.0,       +1),
+        (+np.pi,     0.0,       -1),
+        (-np.pi,     0.0,       -1),
+        (+np.pi / 2, np.pi / 2, -1),
+        (-np.pi / 2, np.pi / 2, +1),
+        (0.125,     -0.125,     +1),
+    ])
+def test_corrector_converter_canonicalizes_thin_dipole_rotations(
+        parsed_elements,
+        xsuite_environment,
+        sad2xs_config,
+        assert_environment_element,
+        sad_rotation,
+        expected_rotation,
+        expected_sign):
+    """
+    Thin SAD corrector rotations should canonicalize the dipole kick.
+    """
+    convert_correctors(
+        parsed_elements = parsed_elements(
+            element_type        = "bend",
+            element_name        = "test_corr",
+            element_variables   = {
+                "angle":    0.0,
+                "k0":       0.1,
+                "k1":       0.2,
+                "rotate":   sad_rotation,
+            }),
+        environment     = xsuite_environment,
+        config          = sad2xs_config)
+
+    ele = assert_environment_element(
+        environment     = xsuite_environment,
+        element_name    = "test_corr",
+        element_type    = xt.Multipole)
+
+    assert ele.knl[0] == pytest.approx(expected_sign * 0.1), (
+        "Thin corrector knl[0] should include the canonical dipole field sign.")
+    assert ele.knl[1] == pytest.approx(0.2), (
+        "Thin corrector K1 should not be flipped by dipole canonicalization.")
+    assert ele.rot_s_rad == pytest.approx(expected_rotation), (
+        "Thin corrector should use the canonical Xsuite dipole rotation.")
 
 def test_corrector_converter_without_length_creates_thin_multipole(
         parsed_elements,
@@ -528,7 +589,7 @@ def test_corrector_pipeline_preserves_names_order_lengths_and_kicks(write_lattic
         MOMENTUM    = 1.0 GEV;
 
         BEND        CH          = (L = 0.5 ANGLE = 0.0 K0 = 0.1)
-                    CV          = (L = 0.5 ANGLE = 0.0 K0 = -0.1 ROTATE = 90 DEG)
+                    CV          = (L = 0.5 ANGLE = 0.0 K0 = -0.1)
                     CZ          = (L = 0.5 ANGLE = 0.0 K0 = 0.0);
 
         MARK        START       = ()
@@ -558,9 +619,9 @@ def test_corrector_pipeline_preserves_names_order_lengths_and_kicks(write_lattic
             f"Converted corrector '{corr_name}' should preserve integrated "
             "kick divided by length.")
 
-def test_corrector_pipeline_preserves_offsets_and_rotation(write_lattice):
+def test_corrector_pipeline_canonicalizes_offsets_and_rotation(write_lattice):
     """
-    Full conversion should preserve corrector DX, DY, and ROTATE fields.
+    Full conversion should preserve corrector offsets and canonicalize ROTATE.
     """
     lattice_path = write_lattice(
         """\
@@ -580,7 +641,7 @@ def test_corrector_pipeline_preserves_offsets_and_rotation(write_lattice):
 
         LINE        TEST_LINE   = (START COFF END);
         """,
-        filename = "corrector_pipeline_preserves_offsets_rotation.sad")
+        filename = "corrector_pipeline_canonicalizes_offsets_rotation.sad")
 
     line = s2x.convert_sad_to_xsuite(
         sad_lattice_path    = str(lattice_path),
@@ -596,8 +657,10 @@ def test_corrector_pipeline_preserves_offsets_and_rotation(write_lattice):
         "Converted corrector should preserve SAD DX as Xsuite shift_x.")
     assert line["coff"].shift_y == pytest.approx(-2.0E-3), (
         "Converted corrector should preserve SAD DY as Xsuite shift_y.")
-    assert line["coff"].rot_s_rad == pytest.approx(-np.pi / 2), (
-        "Converted corrector should apply the SAD-to-Xsuite rotation sign.")
+    assert line["coff"].k0 == pytest.approx(-0.2), (
+        "Converted corrector k0 should include the canonical dipole field sign.")
+    assert line["coff"].rot_s_rad == pytest.approx(np.pi / 2), (
+        "Converted corrector should use the canonical Xsuite vertical dipole rotation.")
 
 ################################################################################
 # Physics Equivalence

tests/writer/elements/test_corr_writer.py
@@ -146,8 +146,8 @@ def _build_scorr_line(
         rot_s_rad = np.pi / 4):
     """
     Build a minimal single skew-corrector Xsuite line.
-    Skew correctors have a rot_s_rad not in {0, ±π/2, π}; the writer places
-    the rotation on the clone as a literal string expression.
+    Skew correctors have a rot_s_rad not in {0, +π/2}; the writer places the
+    rotation on the clone as a literal string expression.
     """
     line = xt.Line(
         elements      = [xt.Marker(), xt.Bend(
@@ -436,9 +436,8 @@ def test_corr_writer_vertical_corr_preserves_rotation(tmp_path):
 ################################################################################
 def test_corr_writer_skew_corr_reloads_as_xsuite_bend(tmp_path):
     """
-    A skew corrector (rot_s_rad not in {0, ±π/2, π}) should reload as an
-    xt.Bend. The writer places the rotation on the clone as a literal string
-    expression.
+    A skew corrector (rot_s_rad not in {0, +π/2}) should reload as an xt.Bend.
+    The writer places the rotation on the clone as a literal string expression.
     """
     original_line = _build_scorr_line()
     reloaded_line = _writer_roundtrip(line = original_line, tmp_path = tmp_path)
@@ -475,6 +474,26 @@ def test_corr_writer_skew_corr_preserves_rotation(tmp_path):
     assert reloaded_line["sc1"].rot_s_rad == pytest.approx(rot), (
         "Writer roundtrip should preserve skew corrector rot_s_rad. "
         f"Original: {rot}, reloaded: {reloaded_line['sc1'].rot_s_rad}.")
+
+
+@pytest.mark.parametrize("rot_s_rad", [np.pi, -np.pi / 2])
+def test_corr_writer_preserves_explicit_special_rotations(tmp_path, rot_s_rad):
+    """
+    External Xsuite correctors with pi or -pi/2 rotations should be written as
+    that representation, not rewritten into SAD2XS conversion canonical form.
+    """
+    original_line = _build_scorr_line(
+        k0        = 1.5E-4,
+        length    = 0.3,
+        rot_s_rad = rot_s_rad)
+    reloaded_line = _writer_roundtrip(line = original_line, tmp_path = tmp_path)
+
+    assert reloaded_line["sc1"].k0 == pytest.approx(1.5E-4), (
+        "Writer roundtrip should preserve explicit-rotation corrector k0. "
+        f"Original: 1.5E-4, reloaded: {reloaded_line['sc1'].k0}.")
+    assert reloaded_line["sc1"].rot_s_rad == pytest.approx(rot_s_rad), (
+        "Writer roundtrip should preserve explicit corrector rot_s_rad. "
+        f"Original: {rot_s_rad}, reloaded: {reloaded_line['sc1'].rot_s_rad}.")
 
 
 ################################################################################